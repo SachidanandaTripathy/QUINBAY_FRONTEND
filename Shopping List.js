444720a9a06ad3926e3ac8f318246f98ff554e62
SachidanandaTripathy <sachidananda.tripathy03@gmail.com>
2024-11-23 23:47:01 +0530

    few more problem solved

---
+++ Shopping List.js
@@ -0,0 +1,7 @@
+const myList = [];
+myList.push(["Sachidananda Tripathy",1]);
+myList.push(["Sasanka Tripathy",2]);
+myList.push(["Sandhya Rani Tripathy",3]);
+myList.push(["Sitha MahaLakshmi",4]);
+myList.push(["Sita",5]);
+console.log(myList);
